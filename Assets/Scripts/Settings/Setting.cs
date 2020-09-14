66cfdc63c49e7bbf835754af0090893ca4d5356d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-09-14 22:46:24 +0900

    バージョン管理するべきではない設定情報を取り扱う為の規定クラスとインタフェースを定義。

---
+++ Assets/Scripts/Settings/Setting.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 設定インタフェース
+/// </summary>
+public interface ISetting
+{
+    /// <summary>
+    /// APIキー
+    /// </summary>
+    string APIKey { get; }
+}
+
+/// <summary>
+/// 設定
+/// </summary>
+public class Setting
+{
+
+    /// <summary>
+    /// APIキー
+    /// </summary>
+    public string APIKey { get { return Interface.APIKey; } }
+
+#region Interface Access
+    protected ISetting Interface
+    {
+        set { _Interface = value; }
+        get
+        {
+            if(_Interface == null) { throw new SettingInterfaceNotSetException(); }
+            return _Interface;
+        }
+    }
+    private ISetting _Interface = null;
+#endregion
+
+#region Singleton
+    public static Setting Instance { get { return _Instance; } }
+    private static Setting _Instance = new Setting();
+    private Setting() {}
+#endregion
+}
+
+/// <summary>
+/// SettingクラスにISettingインタフェースが設定されていないときに投げられる例外
+/// </summary>
+public class SettingInterfaceNotSetException : Exception
+{
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public SettingInterfaceNotSetException()
+        : base("SettingクラスにISettingインタフェースが設定されていません。")
+    {   
+    }
+}
