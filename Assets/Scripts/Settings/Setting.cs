a6860d69b98e1001ff9ea66ad712a796286be596
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-09-14 23:09:08 +0900

    インタフェースを廃止して、Settingクラスそのものに抽象プロパティを持たせるようにした。

--- Assets/Scripts/Settings/Setting.cs
+++ Assets/Scripts/Settings/Setting.cs
@@ -4,57 +4,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// 設定インタフェース
-/// </summary>
-public interface ISetting
-{
-    /// <summary>
-    /// APIキー
-    /// </summary>
-    string APIKey { get; }
-}
-
-/// <summary>
 /// 設定
 /// </summary>
-public class Setting
+public abstract class Setting
 {
-
     /// <summary>
     /// APIキー
     /// </summary>
-    public string APIKey { get { return Interface.APIKey; } }
+    public abstract string APIKey { get; }
 
-#region Interface Access
-    protected ISetting Interface
-    {
-        set { _Interface = value; }
-        private get
-        {
-            if(_Interface == null) { throw new SettingInterfaceNotSetException(); }
-            return _Interface;
-        }
-    }
-    private ISetting _Interface = null;
-#endregion
-
-#region Singleton
-    public static Setting Instance { get { return _Instance; } }
-    protected static Setting _Instance { private get; set; }
+#region Access
+    public static Setting Instance { get { return StaticInstance; } }
+    protected static Setting StaticInstance { private get; set; }
     protected Setting() {}
 #endregion
-}
-
-/// <summary>
-/// SettingクラスにISettingインタフェースが設定されていないときに投げられる例外
-/// </summary>
-public class SettingInterfaceNotSetException : Exception
-{
-    /// <summary>
-    /// コンストラクタ
-    /// </summary>
-    public SettingInterfaceNotSetException()
-        : base("SettingクラスにISettingインタフェースが設定されていません。")
-    {   
-    }
 }
