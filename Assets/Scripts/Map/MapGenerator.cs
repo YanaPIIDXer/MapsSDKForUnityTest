cb4a855210459d57df860a0387c2a86a47d960d6
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-09-15 06:33:48 +0900

    マップ生成に使うComponentを定義。 ※中身はまだ未実装。

---
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Google.Maps;
+
+/// <summary>
+/// マップ生成Component
+/// </summary>
+[RequireComponent(typeof(MapsService))]
+public class MapGenerator : MonoBehaviour
+{
+}
