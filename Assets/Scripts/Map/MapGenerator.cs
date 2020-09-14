34439241cff51a4c0112e7861b6923baae320c20
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-09-15 07:47:56 +0900

    マップ生成機能を実装。 ※デフォルトは秋葉原。

--- Assets/Scripts/Map/MapGenerator.cs
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Google.Maps;
+using Google.Maps.Coord;
+using Google.Maps.Examples.Shared;
 
 /// <summary>
 /// マップ生成Component
@@ -15,9 +17,30 @@ public class MapGenerator : MonoBehaviour
     /// </summary>
     private MapsService Service = null;
     
+    /// 緯度
+    /// </summary>
+    [SerializeField]
+    private float Lat = 35.698683f;
+
+    /// <summary>
+    /// 経度
+    /// </summary>
+    [SerializeField]
+    private float Lng = 139.77421900000002f;
+
     protected void Awake()
     {
-        Service = gameObject.GetComponent<MapsService>();            
+        Service = GetComponent<MapsService>();            
         Service.ApiKey = LocalEnvironments.APIKey;
+        // この辺はStartメソッドでやらないと駄目らしい。
+        //Service.InitFloatingOrigin(new LatLng(Lat, Lng));
+        //Service.LoadMap(DefaultBounds, DefaultGameObjectOptions);
+    }
+
+    protected void Start()
+    {
+        Service.InitFloatingOrigin(new LatLng(Lat, Lng));
+        // ↓Exampleは使いたくなかったが、GameObjectOptionsの構築が面倒だったので止む無く失敬。
+        Service.LoadMap(ExampleDefaults.DefaultBounds, ExampleDefaults.DefaultGameObjectOptions);
     }
 }
